d8a506303e181ddac6214bff87db2fed35635aac
Gyan Kumar <83368867+GyanKumar62065@users.noreply.github.com>
2024-03-18 22:49:08 +0530

    Time: 175 ms (99.81%) | Memory: 92.8 MB (97.46%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.cpp
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int findMinArrowShots(vector<vector<int>>& points) {
+        ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
         sort(points.begin() , points.end());
 
