ce6a1d69a88d1ba6d8ff431588fe3fab70a6940a
Gyan Kumar <83368867+GyanKumar62065@users.noreply.github.com>
2024-06-02 03:45:11 +0530

    Time: 5 ms (99.87%) | Memory: 42.6 MB (7.61%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.java
+++ 3379-score-of-a-string/score-of-a-string.java
@@ -1,10 +1,12 @@
 class Solution {
     public int scoreOfString(String s) {
 
-        int ans = 0;
-        for(int i =0; i < s.length() - 1; i++){
-            ans += Math.abs(s.charAt(i) - s.charAt(i+1));
-        }
-        return ans;
+        // int ans = 0;
+        // for(int i =0; i < s.length() - 1; i++){
+        //     ans += Math.abs(s.charAt(i) - s.charAt(i+1));
+        // }
+        // return ans;
+
+        return java.util.stream.IntStream.range(0 , s.length() - 1).map(i -> Math.abs(s.charAt(i) - s.charAt(i+1))).sum();
     }
 }
