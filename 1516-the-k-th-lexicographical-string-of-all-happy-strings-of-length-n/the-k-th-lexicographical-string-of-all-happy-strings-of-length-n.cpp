4956e7dccc66aef3a9d776a88d4387b262207419
Gyan Kumar <83368867+GyanKumar62065@users.noreply.github.com>
2025-02-19 15:38:16 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (90.21%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,18 +1,34 @@
 class Solution {
 public:
-    string getHappyString(int n, int &k, int i = 0, char last = 0) {
-        if(i == n){
-            k--;
-        }else{
-            for(char ch = 'a'; ch <= 'c'; ch++){
-                if(ch != last){
-                    string res = getHappyString(n , k, i+1, ch);
-                    if(k == 0){
-                        return string(1, ch) + res; // string of size 1 of charchter ch
-                    }
-                }
-            }
+// DFS
+    // string getHappyString(int n, int &k, int i = 0, char last = 0) {
+    //     if(i == n){
+    //         k--;
+    //     }else{
+    //         for(char ch = 'a'; ch <= 'c'; ch++){
+    //             if(ch != last){
+    //                 string res = getHappyString(n , k, i+1, ch);
+    //                 if(k == 0){
+    //                     return string(1, ch) + res; // string of size 1 of charchter ch
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     return "";
+    // }
+
+    string getHappyString(int n, int k) {
+      
+        int perm = (1 << (n-1)); // pow(2 , n-1)
+        if(k > 3 * perm) return "";
+
+        string s = string(1, 'a' + (k-1)/ perm);
+
+        while(perm > 1){
+            k = (k-1) % perm + 1;
+            perm >>= 1; 
+            s += (k-1)/ perm == 0 ? 'a' + (s.back() =='a') : 'b' + (s.back() != 'c');
         }
-        return "";
+        return s;
     }
 };
