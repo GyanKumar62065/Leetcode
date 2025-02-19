8d725110b008607a05ee1e868bcad36b34daf212
Gyan Kumar <83368867+GyanKumar62065@users.noreply.github.com>
2025-02-19 16:31:54 +0530

    Time: 112 ms (7.35%) | Memory: 83.3 MB (5.31%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -1,18 +1,44 @@
 class Solution {
 public:
+    // int firstMissingPositive(vector<int>& nums) {
+    //     sort(nums.begin(),nums.end());
+    //     int i=0;
+    //     while(i < nums.size() && nums[i] <= 0 ){
+    //         i++;
+    //     }
+    //     if(i < nums.size() && (nums[i] != 1) ) return 1;
+    //     for(i; i < nums.size()-1; i++){
+    //         int diff = nums[i+1]-nums[i];
+    //         if(diff>1){
+    //             return nums[i]+1;
+    //         }
+    //     }
+    //     return nums.back() > 0 ? nums.back() + 1 : 1;
+    // }
+
     int firstMissingPositive(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int i=0;
-        while(i < nums.size() && nums[i] <= 0 ){
-            i++;
-        }
-        if(i < nums.size() && (nums[i] != 1) ) return 1;
-        for(i; i < nums.size()-1; i++){
-            int diff = nums[i+1]-nums[i];
-            if(diff>1){
-                return nums[i]+1;
+        set<int>st;
+        for(auto it : nums){
+            if(it > 0){
+                st.insert(it);
             }
         }
-        return nums.back() > 0 ? nums.back() + 1 : 1;
+        vector<int>temp;
+        for(auto it : st){
+            temp.push_back(it);
+        }
+
+        cout << temp.size() << endl;
+        if(temp.empty() || temp[0] > 1) return 1;
+        int ans = temp[0];
+        for(int i = 0; i < temp.size()-1;i++){
+            if(temp[i+1] - temp[i] > 1){
+                ans = temp[i]+1;
+                cout << "Here";
+                break;
+            }
+        }
+        if(ans == temp[0]) ans = temp.back() + 1;
+        return ans;
     }
 };
