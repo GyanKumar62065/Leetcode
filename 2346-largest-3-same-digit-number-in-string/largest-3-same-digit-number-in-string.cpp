4ea9b1538c34c4d67dea524a73e6285a0b779568
Gyan Kumar <83368867+GyanKumar62065@users.noreply.github.com>
2025-08-14 23:33:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (77.37%) - LeetSync

--- 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
@@ -4,10 +4,7 @@ public:
         string ans = "";
         for(int i =0; i < num.size() - 2; i++){
             if(num[i] == num[i+1] && num[i+1] == num[i+2]){
-                string s = "";
-                s += num[i];
-                s += num[i+1];
-                s += num[i+2];
+                string s = num.substr(i , 3);
                 if(ans < s){
                     ans = s;
                 }
